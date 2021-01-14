3e6922b291a8453c2b93592e9414ec94509b07d9
Yasith Amarasinghe <yasith.18@cse.mrt.ac.lk>
2021-01-14 11:09:20 +0530

    Views

--- routes/manager_functions/index.js
+++
@@ -1,8 +0,0 @@
-const employee = require('./employee');
-const food_item = require('./food_items')
-const router = require('express').Router();
-
-router.use('/employee', employee);
-router.use('/food_items', food_item);
-
-module.exports = router;
