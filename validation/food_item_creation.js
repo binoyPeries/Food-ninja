06e5714ef4492fcdebbb4648dac460e62e8a1289
binoy <binoy.18@cse.mrt.ac.lk>
2021-01-19 20:57:33 +0530

    add meal ui

--- validation/food_item_creation.js
+++ validation/food_item_creation.js
@@ -8,8 +8,8 @@ function validateFoodItem(item){
         'food_item_id'          : Joi.string().required().alphanum().max(30),
         'food_item_name'      : Joi.string().required().alphanum().max(30),
         'price'               : Joi.number().required().precision(2).positive(),
-        'description'         : Joi.string().required().max(100),
         'calorie_amount'      : Joi.number().required().precision(2).positive(),
+        'description'         : Joi.string().required().max(100),
         'image'              : Joi.string()
         //joi image validation
         
