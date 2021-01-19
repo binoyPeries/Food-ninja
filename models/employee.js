c000bff3fe88779598b1359e9d59568a862245b9
Sandaru Kaveesha <59358245+sandaru2457@users.noreply.github.com>
2021-01-19 07:41:19 +0530

    test

---
+++ models/employee.js
@@ -0,0 +1,51 @@
+const {pool} = require('../startup/mysql_database');
+module.exports= class Employee {
+
+    //Accept orders 
+    static acceptOrders(body) {
+        return new Promise((resolve, reject) => {
+            pool.query("CALL create_employee (?,?,?)",
+                [
+                    body.employee_name,
+                    body.job_post,
+                    body.contact_number,
+                ],
+                function (error, results, fields) {
+                    if (error) {
+                        reject(error);
+                    };
+                    resolve(console.log("entered sucessfully"));
+                }
+            )
+        })
+    
+        
+    }
+    //Decline Orders
+    static  addFoodItem(body) {
+        return new Promise((resolve, reject) => {
+            pool.query("CALL create_food_item (?,?,?,?,?,?)",
+                [
+                    body.food_item_id,
+                    body.food_item_name,
+                    body.price,
+                    body.description,
+                    body.calorie_amount,
+                    body.image,
+    
+                ],
+                function (error, results, fields) {
+                    if (error) {
+                        reject(error);
+                    };
+                    resolve(console.log("entered sucessfully"));
+                }
+            )
+        })
+    
+        
+    }
+    
+
+}
+
